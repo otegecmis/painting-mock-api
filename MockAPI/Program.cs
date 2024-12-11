03e4dbf91bd7e9b6d4c023a44eef1c5bdee11e7d
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-12-12 01:25:01 +0300

    chore: create a `project`

---
+++ MockAPI/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Painting Mock API");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
