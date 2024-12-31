f2c6e30fbe6f4c525363f6e8a5e9d231dbe7d335
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-12-31 21:00:22 +0300

    refactor: apply `formatting` across the codebase

--- Painting.MockAPI/Dtos/Artist/CreateArtistDto.cs
+++ Painting.MockAPI/Dtos/Artist/CreateArtistDto.cs
@@ -3,5 +3,5 @@ using System.ComponentModel.DataAnnotations;
 namespace Painting.MockAPI.Dtos.Artist;
 
 public record class CreateArtistDto(
-    [Required] [StringLength(50)] string Name
+    [Required][StringLength(50)] string Name
 );
--- Painting.MockAPI/Dtos/Artist/UpdateArtistDto.cs
+++ Painting.MockAPI/Dtos/Artist/UpdateArtistDto.cs
@@ -3,5 +3,5 @@ using System.ComponentModel.DataAnnotations;
 namespace Painting.MockAPI.Dtos.Artist;
 
 public record class UpdateArtistDto(
-    [Required] [StringLength(50)] string Name
+    [Required][StringLength(50)] string Name
 );
